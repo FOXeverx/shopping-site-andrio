c0472181eb0c98dba86118d64ccbde272fc6f285
FOXeverx <3541692950@qq.com>
2026-05-06 18:25:35 +0800

    fix: downgrade AGP 9.1.1 to 8.7.3 for aliyun mirror compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.shopping_site_andrio"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.shopping_site_andrio"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
